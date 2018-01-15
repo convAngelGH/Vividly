ae14a668cffd7110e4adadb5bd0826a8f6c60632
convAngelGH <angel@cmsg.com.au>
2018-01-15 13:56:58 +0800

    1st Commit

---
+++ Vividly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vividly.Models;
+
+namespace Vividly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Name = "Lord of the Rings : Fellowship of the Ring" };
+            return View();
+        }
+    }
+}
---
+++ Vividly/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vividly.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
